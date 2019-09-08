5b7f22a6a5f268b85299447140f2df789481a3ad
EiSandarWin <eisandarwin20@gmail.com>
2019-09-08 12:01:37 +0630

    tax calculator

---
+++ tax.py
@@ -0,0 +1,13 @@
+#width = 20
+#height = 5 * 9
+#vol = width * height
+#print(vol)
+
+
+#-------------------------------------
+tax = 50 / 1000
+price = 10000
+ttax = tax * price
+total = ttax + price
+round(total)
+print(total)
